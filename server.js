18757a2df2c6c91731ee80de316b86670a780987
Karan Singh <kar.n@icloud.com>
2017-05-25 17:32:27 -0700

    rerouting trafffic to http instead of https for API compatibility

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const express = require('express'),
   //binding port to env
   port = process.env.PORT || 3000;
 
-
+app.use((req, res, next)=>{
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log('Express server is up on port 3000')
 });
+ 
